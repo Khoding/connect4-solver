7a058923e1ff22b6729423dc1ff3a7b022284920
Khoding <Khoding@users.noreply.github.com>
2026-06-06 22:14:01 +0000

    deploy: b1b4d99393f3a988fb034dc8cdfc18f36b0dbae2

---
+++ _nuxt/Bnr3HR8n.js
@@ -0,0 +1,1 @@
+import e from"./LY85ag2E.js";import r from"./D18kgxE3.js";import o from"./Bj0Ic2NC.js";const f=()=>({legacy:!1,fallbackLocale:"en",messages:{en:e,fr:r,de:o}});export{f as default};
--- _nuxt/Bp97Z1Sn.js
+++
@@ -1,1 +0,0 @@
-import e from"./BX8UhMY7.js";import r from"./C9tW4fJl.js";import o from"./DQo_uNZz.js";const f=()=>({legacy:!1,fallbackLocale:"en",messages:{en:e,fr:r,de:o}});export{f as default};
